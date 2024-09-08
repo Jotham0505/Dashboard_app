3f7779dbda14d49ac73b71d6fdac397780df9eae
Jotham Emmanuel Cheeran <jothamemmanuelcheeran@gmail.com>
2024-09-08 17:23:23 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:dashboard_ui/constants/constants.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: backgroundColor
+      ),
+      //home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+
